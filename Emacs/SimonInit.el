627f920f6592a39491671825ffc496039d41aefb
Simon <Simon.Kolotov@xjetsolar.com>
2014-03-17 17:08:26 +0200

    Corrected scrolling directions

--- Emacs/SimonInit.el
+++ Emacs/SimonInit.el
@@ -86,15 +86,16 @@
 
 
                                         ; Scroll with Alt-Up/Down
+; NB: scrolling down = moving the window up...
 (global-set-key (kbd "<M-up>")   (lambda () (interactive) (scroll-down 1)))
 (global-set-key (kbd "<M-kp-up>")   (lambda () (interactive) (scroll-down 1)))
 (global-set-key (kbd "<M-down>") (lambda () (interactive) (scroll-up 1)))
 (global-set-key (kbd "<M-kp-down>") (lambda () (interactive) (scroll-up 1)))
-
-(global-set-key (kbd "<C-up>")   (lambda () (interactive) (scroll-other-window-down -1)))
-(global-set-key (kbd "<C-kp-up>")   (lambda () (interactive) (scroll-other-window-down -1)))
-(global-set-key (kbd "<C-down>") (lambda () (interactive) (scroll-other-window-down 1)))
-(global-set-key (kbd "<C-kp-down>") (lambda () (interactive) (scroll-other-window-down 1)))
+                                        ; Scroll Other Window with Alt-Up/Down
+(global-set-key (kbd "<C-up>")   (lambda () (interactive) (scroll-other-window-down 1)))
+(global-set-key (kbd "<C-kp-up>")   (lambda () (interactive) (scroll-other-window-down 1)))
+(global-set-key (kbd "<C-down>") (lambda () (interactive) (scroll-other-window-down -1)))
+(global-set-key (kbd "<C-kp-down>") (lambda () (interactive) (scroll-other-window-down -1)))
 
 
                                         ; Change C-arrows to be the same as M-f/b
