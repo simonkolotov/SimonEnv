3a5413571788d2a9b40068e1f9652b9b5941c52b
Simon Kolotov <Simon.Kolotov@xjet3d.com>
2015-06-18 12:51:17 +0300

    Corrections to MotionBackup Output to backup log file

--- Backup/Backup.sh
+++ Backup/Backup.sh
@@ -1,11 +1,10 @@
 # copy this file to /home/simon/scripts/Backup.sh
 
-if [ -f /home/simon/github/SimonEnv/Bash/SimonWorkBackup ]; then
-	  source /home/simon/github/SimonEnv/Bash/SimonWorkBackup
-else
-    echo Backup Command Missing
-fi
+TimeStamp=$(date +%Y-%m-%d)
+BackupLog=/home/simon/Incoming/Backup/Backup-$TimeStamp.log
 
-#if [ -f /home/simon/github/SimonEnv/Bash/SimonHomeBackup ]; then
-#	source /home/simon/github/SimonEnv/Bash/SimonHomeBackup
-#fi
+if [ -f /home/simon/github/SimonEnv/Bash/SimonWorkBackup ]; then
+	  source /home/simon/github/SimonEnv/Bash/SimonWorkBackup &> $BackupLog
+else
+    echo Backup Command Missing &> $BackupLog
+fi
