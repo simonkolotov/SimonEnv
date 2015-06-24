e7e013ce9be00325c0903713b8083a8b52dfe89d
Simon Kolotov <Simon.Kolotov@xjet3d.com>
2015-06-24 12:11:27 +0300

    Work Done on Backup Scripts

--- Backup/Backup.sh
+++ Backup/Backup.sh
@@ -2,9 +2,11 @@
 
 TimeStamp=$(date +%Y-%m-%d)
 BackupLog=/home/simon/Incoming/Backup/Backup-$TimeStamp.log
+MotionBackupLog=/home/simon/Incoming/Backup/MotionBackup-$TimeStamp.log
 
 if [ -f /home/simon/github/SimonEnv/Bash/SimonWorkBackup ]; then
 	  source /home/simon/github/SimonEnv/Bash/SimonWorkBackup &> $BackupLog
+    source /home/simon/github/SimonEnv/Bash/SimonWorkMotionBackup &> $MotionBackupLog
 else
     echo Backup Command Missing &> $BackupLog
 fi
