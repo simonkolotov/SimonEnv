66abaf5e00d1862b9b35610d2f2c6dfe4708e7c6
Simon Kolotov <Simon.Kolotov@xjet3d.com>
2015-06-24 12:13:36 +0300

    Minor Corrections to Backup.sh

--- Backup/Backup.sh
+++ Backup/Backup.sh
@@ -1,8 +1,8 @@
 # copy this file to /home/simon/scripts/Backup.sh
 
 TimeStamp=$(date +%Y-%m-%d)
-BackupLog=/home/simon/Incoming/Backup/Backup-$TimeStamp.log
-MotionBackupLog=/home/simon/Incoming/Backup/MotionBackup-$TimeStamp.log
+BackupLog=/home/simon/Backup/Backup-$TimeStamp.log
+MotionBackupLog=/home/simon/Backup/MotionBackup-$TimeStamp.log
 
 if [ -f /home/simon/github/SimonEnv/Bash/SimonWorkBackup ]; then
 	  source /home/simon/github/SimonEnv/Bash/SimonWorkBackup &> $BackupLog
