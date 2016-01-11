1a7cc0f8d5610e87505cfcb08ea54a6ba9167f60
Simon Kolotov <simon@simonpc.XJETDOM.local>
2016-01-11 10:37:42 +0200

    updated backup script

--- Backup/Backup.sh
+++ Backup/Backup.sh
@@ -9,6 +9,8 @@ TimeStamp=$(date --date="yesterday" +%Y-%m-%d)
 BackupLog=/home/simon/Backup/Backup-$TimeStamp.log
 MotionBackupLog=/home/simon/Backup/MotionBackup-$TimeStamp.log
 
+echo "Logging to " $BackupLog
+
 if [ -f /home/simon/github/SimonEnv/Backup/SimonWorkBackup ]; then
     source /home/simon/github/SimonEnv/Backup/SimonWorkBackup &> $BackupLog
     source /home/simon/github/SimonEnv/Backup/SimonWorkMotionBackup &> $MotionBackupLog
