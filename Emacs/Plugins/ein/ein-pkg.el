cc2eab41a89e45485212a514100395006281e9d3
Simon Kolotov <simonkolotov@gmail.com>
2017-12-01 11:47:18 -0800

    more cleanup of the Plugins dir, added Smex for smart M-x command using IDO mode

--- Emacs/Plugins/ein/ein-pkg.el
+++
@@ -1,6 +0,0 @@
-(define-package "ein"
-  "0.3.0"
-  "Emacs IPython Notebook"
-  '((websocket "1.3")
-    (request "0.2")))
-    
--- Emacs/SimonInit.el
+++ Emacs/SimonInit.el
@@ -135,6 +135,16 @@
 (require 'xmsi-math-symbols-input)
 (xmsi-mode)
 
+;;;;;;;;;;smex for using IDO for M-x commands
+(require 'smex) ; Not needed if you use package.el
+  (smex-initialize) ; Can be omitted. This might cause a (minimal) delay
+                                        ; when Smex is auto-initialized on its first run.
+
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+;; This is your old M-x.
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+
 ;;;;;;;;;;Undo-Tree
 (require 'undo-tree)
 (global-undo-tree-mode)
